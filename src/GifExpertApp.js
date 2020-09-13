daf995bb045ce4603c6afe90d0713aeff293fa58
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-13 15:21:56 -0500

    Se agrega boton de agregar nueva categoria
    	modificado:     src/GifExpertApp.js

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+  //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+  const [categories, setCategories] = useState([
+    'One Punch',
+    'Samurai X',
+    'Dragon Ball',
+  ]);
+
+  const handleAdd = () => {
+    //setCategories(['Saint Seiya', ...categories]); // ambas formas sirven
+    setCategories((cats) => ['Saint Seiya', ...cats]);
+  };
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+      <button onClick={handleAdd}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
