938c196fcadb53fdeca3b17663312588cea65b3d
Carlos Barcos <fbarcos8@hotmail.com>
2020-09-14 13:34:38 -0500

    docs agregado
    	nuevo archivo:  docs/

---
+++ docs/precache-manifest.e9b67def375923c374dbd4ede1034a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c570a247fc5ac601005e4f3c0a26ee56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d2e293a987651808802",
+    "url": "/static/css/main.26cc6001.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2e293a987651808802",
+    "url": "/static/js/main.6e277054.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
