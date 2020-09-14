c6766dd57647d3508315a37bdc2fd14f2cfff5be
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-14 09:02:22 -0500

    Usando useFetchGifs para obtener imagenes y bandera de loading...
    	modificado:     src/components/GifGrid.js
    	modificado:     src/hooks/useFetchGifs.js

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import { useFetchGifs } from '../hooks/useFetchGifs';
-//import { getGifs } from '../helpers/getGifs';
-//import { GifGridItem } from './GifGridItem';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-  /*const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getGifs(category).then((imgs) => setImages(imgs));
-  }, [category]);*/
-  const { loading } = useFetchGifs();
-  console.log(loading);
+  const { data: images, loading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
-      {loading ? 'Cargando...' : 'Data cargada'}
-      {/* <div className="card-grid">
+      {loading && <p>Loading</p>}
+      <div className="card-grid">
         {images.map((img) => (
           <GifGridItem key={img.id} {...img} />
         ))}
-      </div> */}
+      </div>
     </>
   );
 };
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,6 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getGifs } from '../helpers/getGifs';
 
-export const useFetchGifs = () => {
-  const [state, setstate] = useState({ data: [], loading: true });
+export const useFetchGifs = (category) => {
+  const [state, setstate] = useState({
+    data: [],
+    loading: true,
+  });
+
+  // se usa use effect para evitar que cada vez que un estado cambie... se renderize todo
+  useEffect(() => {
+    setTimeout(() => {
+      getGifs(category).then((imgs) =>
+        setstate({
+          data: imgs,
+          loading: false,
+        })
+      );
+    }, 3000);
+  }, [category]);
   return state;
 };
