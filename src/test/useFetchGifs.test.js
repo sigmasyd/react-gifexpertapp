2709f2d360e89deb6968dd09fde102231f3162d9
Carlos Barcos <fbarcos8@hotmail.com>
2020-09-24 01:15:34 -0500

    Se agrega test para useEfect y con await para esperar tarea sincrona para cambiar al estado
    	modificado:     src/test/__snapshots__/GifGrid.test.js.snap
    	modificado:     src/test/useFetchGifs.test.js

--- src/test/useFetchGifs.test.js
+++ src/test/useFetchGifs.test.js
@@ -3,15 +3,30 @@ import { renderHook } from '@testing-library/react-hooks';
 import '@testing-library/jest-dom';
 
 describe('Pruebas en el hook useFetchGifs', () => {
-  test('debe de retornar el estado inicial', () => {
+  test('debe de retornar el estado inicial', async () => {
     const category = 'pokemon';
-    const { result } = renderHook(() => useFetchGifs(category));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs(category)
+    );
     const { data, loading } = result.current;
 
     //const { data, loading } = useFetchGifs(category);
     //console.log(data, loading);
 
+    await waitForNextUpdate();
+
     expect(data).toEqual([]);
     expect(loading).toBe(true);
+  });
+  test('debe de retornar areglo de imagenes y loading false', async () => {
+    const category = 'pokemon';
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs(category)
+    );
+    await waitForNextUpdate();
+
+    const { data, loading } = result.current;
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
   });
 });
