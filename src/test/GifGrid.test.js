8603660bdf77396e91e15bac8e65c3a5d7819415
Carlos Barcos <fbarcos8@hotmail.com>
2020-09-23 23:56:04 -0500

    Se evaluan el componente GifGrid de forma mas estricta verificando si existen otros componentes...
    	modificado:     src/test/GifGrid.test.js
    	modificado:     src/test/__snapshots__/GifGrid.test.js.snap

--- src/test/GifGrid.test.js
+++ src/test/GifGrid.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { GifGrid } from '../components/GifGrid';
+
+// no es necesario porque se importa por defecto, pero para que visual studio code
+// detecte y muestre el autocompletado de la ayuda
 import '@testing-library/jest-dom';
+
 import { useFetchGifs } from '../hooks/useFetchGifs';
 jest.mock('../hooks/useFetchGifs');
 
@@ -24,12 +28,25 @@ describe('Pruebas sobre el componente <GifGrid />', () => {
         url: 'https://asdasdas',
         title: 'Cualquier cosas',
       },
+      {
+        id: 'DEF',
+        url: 'https://asdasdas',
+        title: 'Cualquier cosa',
+      },
     ];
     useFetchGifs.mockReturnValue({
       data: gifs,
       loading: false,
     });
     const wrapper = shallow(<GifGrid category={category} />);
-    expect(wrapper).toMatchSnapshot();
+    // sirve pero es mejor ser mas especificoo: en este caso evaluaremos
+    // si se crea con el numero exacto de imagenes que se recibe....
+    //expect(wrapper).toMatchSnapshot();
+
+    // expect(wrapper).toMatchSnapshot();  // realizar pruebas en mas profuncidad
+    // si las imagenes cargaron.. se esperaria que no exista ni un solo parrafo (solo hay
+    // un parrafo que es donde caraga el loading)
+    expect(wrapper.find('p').exists()).toBe(false);
+    expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
   });
 });
